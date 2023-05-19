e803d3545c24a9f6b5a3b035923b6287e95388e5
causeway-rms <118050291+causeway-rms@users.noreply.github.com>
2023-05-19 13:30:18 +0530

    Modify grid size to be dynamic with user input

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+// Makes the grid size dynamic with user input
+
+const gridSize = prompt('Enter the desired grid size (Must be 1 to 100): ');
+const cellCount = gridSize * gridSize;
+const cellHeight = 100 / gridSize;
+const cellWidth = 100 / gridSize;
+
+// Constructs the grid according to user input
+
 const container = document.querySelector('.container');
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < cellCount; i++) {
     const pixels = document.createElement('div');
     container.appendChild(pixels);
     pixels.setAttribute('class', 'pixels');
+    pixels.style.width = `${cellWidth}%`;
+    pixels.style.height = `${cellHeight}%`;
 }
+
+// Makes the grid cells change color when mouse hovers over
 
 const cells = document.getElementsByClassName('pixels');
 
